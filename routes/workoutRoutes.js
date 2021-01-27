f0c4c1122e90e951c251a568f1181836b3d4323e
wylie <wyliedavid1984@gmail.com>
2021-01-27 09:53:39 -0500

    added limit of query results

--- routes/workoutRoutes.js
+++ routes/workoutRoutes.js
@@ -4,10 +4,10 @@ const Workout = require("../models/workout");
 module.exports = function (app) {
     // route for getting workouts to set up charts
     app.get("/api/workouts/range", (req, res) => {
-        Workout.find({ exercise: {$size:7}}, (err, data) => {
+        Workout.find({}, (err, data) => {
             if (err) throw err;
             res.json(data);
-        });
+        }).limit(7);
     });
     // route to get all workout
     app.get("/api/workouts", (req, res) => {
